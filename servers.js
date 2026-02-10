c7f7c333aee3acff3b7045c55a81c2e309266598
beepboopbeeeep <internetazadvmess.test1400@gmail.com>
2026-02-10 22:05:26 +0330

    Update and rename check-services.js to servers.js

---
+++ servers.js
@@ -0,0 +1,16 @@
+[
+  {
+    "id": 1,
+    "name": "سرویس مرکزی",
+    "url": "https://tellmeimright.taxyvy.workers.dev/panel",
+    "expiry_date": "unlimited",
+    "description": ""
+  },
+  {
+    "id": 2,
+    "name": "سرویس سلطان",
+    "url": "https://hitmeintheyes.judiopu.workers.dev/panel",
+    "expiry_date": "1404/12/21",
+    "description": ""
+  }
+]
